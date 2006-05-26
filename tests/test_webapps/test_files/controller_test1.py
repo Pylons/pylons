9e22eee5bf6019f7a71ba748f03cab7f4967f1a3
bbangert <none@none>
2006-05-26 12:13:01 -0700

    [svn] Updated to use response registered object instead of manually creating a WSGIResponse object
    --HG--
    branch : trunk

--- tests/test_webapps/test_files/controller_test1.py
+++ tests/test_webapps/test_files/controller_test1.py
@@ -2,24 +2,24 @@ from projectname.lib.base import *
 
 class Test1Controller(BaseController):
     def index(self):
-        return WSGIResponse('basic index page')
+        return response('basic index page')
     
     def session_increment(self):
         session.setdefault('counter', -1)
         session['counter'] += 1
         session.save()
-        return WSGIResponse('session incrementer')
+        return response('session incrementer')
     
     def globalup(self):
-        return WSGIResponse(g.message)
+        return response(g.message)
     
     def global_store(self, id):
         if id:
             g.counter += int(id)
-        return WSGIResponse(str(g.counter))
+        return response(str(g.counter))
     
     def myself(self):
-        return WSGIResponse(h.url_for())
+        return response(h.url_for())
     
     def myparams(self):
-        return WSGIResponse(str(params))
+        return response(str(params))
