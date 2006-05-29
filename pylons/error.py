020a9c3cec5d9119851d2471eedc20a5e1632133
bbangert <none@none>
2006-05-29 10:23:00 -0700

    [svn] Removed line-break on error template.
    --HG--
    branch : trunk

--- pylons/error.py
+++ pylons/error.py
@@ -198,7 +198,7 @@ def format_html(exc_data, include_hidden_frames=False, **ops):
     </div>
     """ % (short_er, long_er, cgi.escape(text_er)), ''
 
-error_template = '''
+error_template = '''\
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
