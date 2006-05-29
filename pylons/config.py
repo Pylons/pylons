510cd874ddad5c4dd19cf25f0f0d47b267c15b89
bbangert <none@none>
2006-05-29 13:37:46 -0700

    [svn] Copy session/cache settings into proper locations for middleware.
    --HG--
    branch : trunk

--- pylons/config.py
+++ pylons/config.py
@@ -125,6 +125,10 @@ class Config(object):
             self.template_options['myghty.'+k] = v
             if k.startswith('session_') or k.startswith('cache_'):
                 self.app_conf[k] = v
-        
+        if not app_conf.has_key('session_cache_dir'):
+            app_conf['session_data_dir'] = app_conf['cache_dir'] + '/sessions'
+        if not app_conf.has_key('cache_data_dir'):
+            app_conf['cache_data_dir'] = app_conf['cache_dir']
+        #raise str(app_conf)
         # Save our errorware values
         self.errorware = errorware
