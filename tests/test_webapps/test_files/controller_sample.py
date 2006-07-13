19140f04bc95144d5d7c9984123541f3243d8049
bbangert <none@none>
2006-07-12 21:14:25 -0700

    [svn] Updating test script and new project template to use Response.
    --HG--
    branch : trunk

--- tests/test_webapps/test_files/controller_sample.py
+++ tests/test_webapps/test_files/controller_sample.py
@@ -2,45 +2,45 @@ from projectname.lib.base import *
 
 class SampleController(BaseController):
     def index(self):
-        return response('basic index page')
+        return Response('basic index page')
     
     def session_increment(self):
         session.setdefault('counter', -1)
         session['counter'] += 1
         session.save()
-        return response('session incrementer')
+        return Response('session incrementer')
     
     def globalup(self):
-        return response(g.message)
+        return Response(g.message)
     
     def global_store(self, id):
         if id:
             g.counter += int(id)
-        return response(str(g.counter))
+        return Response(str(g.counter))
     
     def myself(self):
-        return response(h.url_for())
+        return Response(h.url_for())
     
     def myparams(self):
-        return response(str(request.params))
+        return Response(str(request.params))
     
     def testdefault(self):
-        return render_response('testkid')
+        return render_Response('testkid')
 
     def test_extra_engine(self):
-        return render_response('kid', 'testkid')
+        return render_Response('kid', 'testkid')
     
     def test_template_caching(self):
-        return render_response('/test_myghty.myt', cache_expire='never')
+        return render_Response('/test_myghty.myt', cache_expire='never')
     
     def test_only_post(self):
-        return response('It was a post!')
+        return Response('It was a post!')
     test_only_post = rest.dispatch_on(GET='test_only_get')(rest.restrict('POST')(test_only_post))
     
     def test_only_get(self):
-        return response('It was a get!')
+        return Response('It was a get!')
     test_only_get = rest.restrict('GET')(test_only_get)
     
     def impossible(self):
-        return response('This should never be shown')
+        return Response('This should never be shown')
     impossible = rest.restrict('POST')(rest.dispatch_on(POST='test_only_post')(impossible))
