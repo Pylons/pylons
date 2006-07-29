001d10e992fb82db542bce1474c9b8d1870d7dc2
bbangert <none@none>
2006-07-29 16:01:09 -0700

    [svn] Removed lowercasing of the module name during resolver import.
    --HG--
    branch : trunk

--- pylons/wsgiapp.py
+++ pylons/wsgiapp.py
@@ -148,7 +148,7 @@ class PylonsBaseWSGIApp(object):
         # @@ TODO: Encapsulate in try/except to raise error if controller
         #          doesn't exist, or class in controller file doesn't exist.
         full_module_name = self.package_name + '.controllers.' \
-            + controller.lower().replace('/', '.')
+            + controller.replace('/', '.')
         
         __import__(full_module_name)
         module_name = full_module_name.split('.')[-1]
