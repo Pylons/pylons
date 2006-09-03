c36e297104552885ca1b3dfd1417ae7e085b95f8
bbangert <none@none>
2006-09-02 23:26:00 -0700

    [svn] * Re-organized helpers, switched Helpers class to use static methods to reduce code duplication.
    --HG--
    branch : trunk

--- pylons/util.py
+++ pylons/util.py
@@ -13,6 +13,67 @@ from paste.script.templates import Template
 from routes import threadinglocal
 import pylons
 
+def log(msg):
+    """Log a message to the output log."""
+    pylons.request.environ['wsgi.errors'].write('=> %s\n'%str(msg))
+
+def _(value):
+    """Mark a string for translation
+    
+    Mark a string to be internationalized as follows:
+    
+    .. code-block:: Python
+    
+        h._('This should be in lots of languages')
+    
+    """
+    return pylons.translator['translator'].gettext(value)
+
+def set_lang(lang):
+    """Set the language used"""
+    project_name = CONFIG['app_conf']['package']
+    pylons.translator['lang'] = lang
+    if lang is None:
+        pylons.translator['translator'] = _Translator()
+    else:
+        from pkg_resources import resource_string, resource_stream, \
+            resource_exists, resource_filename
+        from pylons.i18n.translation import egg_translation
+        catalog_path = os.path.join('i18n', lang, 'LC_MESSAGES')
+        if not resource_exists(project_name, catalog_path):
+            raise LanguageError('Language catalog %s not found' % \
+                                os.path.join(project_name, catalog_path))
+        pylons.translator['translator'] = \
+            egg_translation(project_name, lang=catalog_path)
+
+def get_lang():
+    return pylons.translator['lang']
+
+def get_prefix(environ):
+    prefix = environ['paste.config']['app_conf'].get('prefix', '')
+    if not prefix:
+        if environ.get('SCRIPT_NAME', '') != '':
+            prefix = environ['SCRIPT_NAME']
+    return prefix
+
+def class_name_from_module_name(module_name):
+    """Takes a module name and returns the name of the class it defines.
+
+    If the module name contains dashes, they are replaced with underscores.
+    
+    Example::
+    
+        >>> class_name_from_module_name('with-dashes')
+        'WithDashes'
+        >>> class_name_from_module_name('with_underscores')
+        'WithUnderscores'
+        >>> class_name_from_module_name('oneword')
+        'Oneword'
+    
+    """
+    words = module_name.replace('-', '_').split('_')
+    return ''.join([w.title() for w in words])
+
 class ContextObj(object):
     """ The 'c' object, with strict attribute access (raises an Exception when
     the attribute does not exist) """
@@ -57,112 +118,20 @@ class Helpers(object):
                                  "context object 'c' to store context data.")
         else:
             self.set_lang(value)
-
-    def log(self, msg):
-        """Log a message to the output log."""
-        pylons.request.environ['wsgi.errors'].write('=> %s\n'%str(msg))
-
+    
     def translate(self, value):
         """Deprecated, use _()"""
         raise NotImplementedError('Use h._() instead')
-
-    def _(self, value):
-        """Mark a string for translation
-        
-        Mark a string to be internationalized as follows:
-        
-        .. code-block:: Python
-        
-            h._('This should be in lots of languages')
-        
-        """
-        return pylons.translator['translator'].gettext(value)
-  
-    def set_lang(self, lang):
-        """Set the language used"""
-        project_name = CONFIG['app_conf']['package']
-        pylons.translator['lang'] = lang
-        if lang is None:
-            pylons.translator['translator'] = _Translator()
-        else:
-            from pkg_resources import resource_string, resource_stream, \
-                resource_exists, resource_filename
-            from pylons.i18n.translation import egg_translation
-            catalog_path = os.path.join('i18n', lang, 'LC_MESSAGES')
-            if not resource_exists(project_name, catalog_path):
-                raise LanguageError('Language catalog %s not found' % \
-                                    os.path.join(project_name, catalog_path))
-            pylons.translator['translator'] = \
-                egg_translation(project_name, lang=catalog_path)
-
-    def get_lang(self):
-        return pylons.translator['lang']
+    
+    log = staticmethod(log)
+    _ = staticmethod(_)
+    set_lang = staticmethod(set_lang)
+    get_lang = staticmethod(get_lang)
 
 class PylonsTemplate(Template):
     _template_dir = 'templates/paster_template'
     summary = 'Pylons application template'
     egg_plugins = ['Pylons', 'WebHelpers']
-
-def get_prefix(environ):
-    prefix = environ['paste.config']['app_conf'].get('prefix', '')
-    if not prefix:
-        if environ.get('SCRIPT_NAME', '') != '':
-            prefix = environ['SCRIPT_NAME']
-    return prefix
-
-def class_name_from_module_name(module_name):
-    """Takes a module name and returns the name of the class it defines.
-
-    If the module name contains dashes, they are replaced with underscores.
-    
-    Example::
-    
-        >>> class_name_from_module_name('with-dashes')
-        'WithDashes'
-        >>> class_name_from_module_name('with_underscores')
-        'WithUnderscores'
-        >>> class_name_from_module_name('oneword')
-        'Oneword'
-    
-    """
-    words = module_name.replace('-', '_').split('_')
-    return ''.join([w.title() for w in words])
-
-def log(msg):
-    """Log a message to the output log."""
-    pylons.request.environ['wsgi.errors'].write('=> %s\n'%str(msg))
-
-def _(value):
-    """Mark a string for translation
-    
-    Mark a string to be internationalized as follows:
-    
-    .. code-block:: Python
-    
-        h._('This should be in lots of languages')
-    
-    """
-    return pylons.translator['translator'].gettext(value)
-
-def set_lang(lang):
-    """Set the language used"""
-    project_name = CONFIG['app_conf']['package']
-    pylons.translator['lang'] = lang
-    if lang is None:
-        pylons.translator['translator'] = _Translator()
-    else:
-        from pkg_resources import resource_string, resource_stream, \
-            resource_exists, resource_filename
-        from pylons.i18n.translation import egg_translation
-        catalog_path = os.path.join('i18n', lang, 'LC_MESSAGES')
-        if not resource_exists(project_name, catalog_path):
-            raise LanguageError('Language catalog %s not found' % \
-                                os.path.join(project_name, catalog_path))
-        pylons.translator['translator'] = \
-            egg_translation(project_name, lang=catalog_path)
-
-def get_lang():
-    return pylons.translator['lang']
 
 __all__ = ['AttribSafeContextObj', 'ContextObj', 'Helpers', 'class_name_from_module_name',
     'log', '_', 'set_lang', 'get_lang']
