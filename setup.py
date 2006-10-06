b67e3827a6c5825e15578fbf938e747226d638dc
bbangert <none@none>
2006-10-06 13:58:31 -0700

    [svn] Updating Paste req to latest stable version.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "Routes>=1.5", "Myghty>=1.1", "Paste>=0.9.8.1",
+        "Routes>=1.5", "Myghty>=1.1", "Paste==dev,>=0.9.9dev-r5793",
         "PasteDeploy>=0.9.6", "PasteScript>=0.9.8", "FormEncode>=0.6",
         "simplejson>=1.4", "WebHelpers>=0.2.1", "nose>=0.9.0", "Beaker>=0.6.1",
     ],
