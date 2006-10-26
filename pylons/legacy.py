39970af71fc2b7d668c7d7727243d575d7a31ac3
bbangert <none@none>
2006-10-26 16:28:37 -0700

    [svn] Added deprecation warnings for using pre-0.9 functions and pre-0.9.3 helper passing.
    --HG--
    branch : trunk

--- pylons/legacy.py
+++ pylons/legacy.py
@@ -5,6 +5,7 @@ Myghty compatibility object, old-style ``params`` and ``m`` globals. The
 """
 import types
 import sys
+import warnings
 
 from paste.registry import StackedObjectProxy
 import paste.httpexceptions as httpexceptions
@@ -23,6 +24,16 @@ def load_h(package_name):
     style Helper imports. The proper style is to pass the helpers module ref
     to the PylonsApp during initialization.
     """
+    warnings.warn(
+        "Pylons 0.9.3 and above now passes the helpers module reference in "
+        "directly. It's highly recommended that you update your middleware.py "
+        "module so that the Pylons app load looks similar to:\n"
+        "app = pylons.wsgiapp.PylonsApp(config, helpers=MYPROJ.helpers)\n"
+        "Where MYPROJ is the name of your project. Also make sure to add an "
+        "import line to the middleware.py to import the helpers modules:\n"
+        "import MYPROJ.helpers\n"
+        "This will be required on all projects 1.0 and beyond.",
+        DeprecationWarning, 2)
     __import__(package_name + '.lib.base')
     their_h = getattr(sys.modules[package_name + '.lib.base'], 'h', None)
     if isinstance(their_h, types.ModuleType):
@@ -64,39 +75,76 @@ class MyghtyCompat(object):
     
     def write(self, content):
         """Write content to the response buffer"""
+        warnings.warn(
+            "The m.write call has been deprecated. Replace with "
+            "response.write() and instantiate a Response object.",
+            DeprecationWarning, 2)
         response.write(content)
     out = write
     
     def cache_self(self, *args, **kargs):
         """Pretends to cache itself, actually disables caching"""
+        warnings.warn(
+            "The m.cache_self call has been deprecated. Replace usage of "
+            "m.cache_self with return render_response() and add cache "
+            "keywords.",
+            DeprecationWarning, 2)
         return False
     
     def subexec(self, *args, **kargs):
         """Renders a template to the response buffer"""
+        warnings.warn(
+            "The m.subexec call has been deprecated. Replace with "
+            "return render_response().",
+            DeprecationWarning, 2)
         response.write(tmpl.render(*args, **kargs))
     
     def comp(self, *args, **kargs):
         """Renders a template fragment to the response buffer"""
+        warnings.warn(
+            "The m.comp call has been deprecated. Replace with return "
+            "render_response('sometemplate.myt', fragment=True) or "
+            "response.write(render('sometemplate.myt', fragment=True)) "
+            "as necessary.",
+            DeprecationWarning, 2)
         response.write(tmpl.render(fragment=True, *args, **kargs))
     
     def scomp(self, *args, **kargs):
         """Returns a rendered template fragment as a string"""
+        warnings.warn(
+            "The m.scomp call has been deprecated. Replace with result = "
+            "render('sometemplate.myt', fragment=True).",
+            DeprecationWarning, 2)
         return tmpl.render(fragment=True, *args, **kargs)
     
     def fetch_component(self, name):
         """Returns the name of the component"""
+        warnings.warn(
+            "The m.fetch_component call has been deprecated. See cache docs.",
+            DeprecationWarning, 2)
         return name
     
     def get_cache(self, component):
         """Returns a cache namespace"""
+        warnings.warn(
+            "The m.get_cache call has been deprecated. See cache docs.",
+            DeprecationWarning, 2)
         return pylons.cache.get_cache(component)
     
     def send_redirect(self, path, hard=True):
         """Raises a redirect exception for Paste HTTPExceptions"""
+        warnings.warn(
+            "The m.send_redirect call has been deprecated. Replace with "
+            "redirect_to()",
+            DeprecationWarning, 2)
         raise httpexceptions.HTTPFound(path)
     
     def abort(self, status_code=None, reason=""):
         """Raises a 404 or the given exception for Paste HTTPExceptions"""
+        warnings.warn(
+            "The m.abort call has been deprecated. Replace with "
+            "abort(200, detail='some message').",
+            DeprecationWarning, 2)
         if status_code == 404:
             raise httpexceptions.HTTPNotFound()
         else:
