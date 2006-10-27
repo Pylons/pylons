efd10bc8db5e0a72927646fdacb0d14ec6dc1ac2
bbangert <none@none>
2006-10-26 17:17:19 -0700

    [svn] * Updated project template to support full_stack option to make it easier to   use Pylons apps within larger WSGI stacks.
    --HG--
    branch : trunk

--- tests/test_webapps/test_files/middleware_def_engine.py
+++ tests/test_webapps/test_files/middleware_def_engine.py
@@ -3,16 +3,17 @@ from paste.cascade import Cascade
 from paste.urlparser import StaticURLParser
 from paste.registry import RegistryManager
 from paste.deploy.config import ConfigMiddleware
+from paste.deploy.converters import asbool
 
 from pylons.error import error_template
-from pylons.middleware import ErrorHandler, ErrorDocuments, error_mapper
+from pylons.middleware import ErrorHandler, ErrorDocuments, StaticJavascripts, error_mapper
 import pylons.wsgiapp
 
 import projectname.lib.helpers
 import projectname.lib.app_globals as app_globals
 from projectname.config.environment import load_environment
 
-def make_app(global_conf, **app_conf):
+def make_app(global_conf, full_stack=True, **app_conf):
     """Create a WSGI application and return it
     
     global_conf is a dict representing the Paste configuration options, the
@@ -36,26 +37,24 @@ def make_app(global_conf, **app_conf):
     app = ConfigMiddleware(app, {'app_conf':app_conf,
         'global_conf':global_conf})
     
-    # YOUR MIDDLEWARE
-    # Put your own middleware here, so that any problems are caught by the error
-    # handling middleware underneath
+    # If errror handling and exception catching will be handled by middleware
+    # for multiple apps, you will want to set full_stack = False in your config
+    # file so that it can catch the problems.
+    if asbool(full_stack):
+        # Change HTTPExceptions to HTTP responses
+        app = httpexceptions.make_middleware(app, global_conf)
     
-    # @@@ Change HTTPExceptions to HTTP responses @@@
-    app = httpexceptions.make_middleware(app, global_conf)
+        # Error Handling
+        app = ErrorHandler(app, global_conf, error_template=error_template, **config.errorware)
     
-    # @@@ Error Handling @@@
-    app = ErrorHandler(app, global_conf, error_template=error_template, **config.errorware)
+        # Display error documents for 401, 403, 404 status codes (if debug is disabled also
+        # intercepts 500)
+        app = ErrorDocuments(app, global_conf, mapper=error_mapper, **app_conf)
     
-    # @@@ Establish the Registry for this application @@@
+    # Establish the Registry for this application
     app = RegistryManager(app)
     
-    # @@@ Static Files in public directory @@@
-    staticapp = StaticURLParser(config.paths['static_files'])
-    
-    # @@@ Cascade @@@ 
-    app = Cascade([staticapp, app])
-    
-    # @@@ Display error documents for 401, 403, 404 status codes (if debug is False also intercepts 500) @@@
-    app = ErrorDocuments(app, global_conf, mapper=error_mapper, **app_conf)
-    
+    static_app = StaticURLParser(config.paths['static_files'])
+    javascripts_app = StaticJavascripts()
+    app = Cascade([static_app, javascripts_app, app])
     return app
