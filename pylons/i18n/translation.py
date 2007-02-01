64ecec563b8fb10a217b390a5a8e4023076583aa
thejimmyg <none@none>
2007-02-01 10:21:40 -0800

    [svn] Add support for fallback languages
    --HG--
    branch : trunk

--- pylons/i18n/translation.py
+++ pylons/i18n/translation.py
@@ -2,17 +2,16 @@
 
 Provides ``gettext`` translation functions via an app's ``pylons.translator``
 and get/set_lang for changing the language translated to."""
+
 import os
-from gettext import NullTranslations, translation
-
-from pkg_resources import resource_exists, resource_stream
-
 import pylons
+
+from gettext import NullTranslations, translation
+from pkg_resources import resource_exists, resource_stream
 
 class LanguageError(Exception):
     """Exception raised when a problem occurs with changing languages"""
     pass
-
 
 def gettext_noop(value):
     """Mark a string for translation without translating it. Returns value.
@@ -95,24 +94,34 @@ def ungettext(singular, plural, n):
     """
     return pylons.translator.ungettext(singular, plural, n)
 
+def _get_translator(lang):
+    """Utility method to get a valid translator object from a language name"""  
+    import pylons.util as util
+    rootdir = pylons.g.pylons_config.paths.get('root_path')
+    localedir = os.path.join(rootdir, 'i18n')
+    if not isinstance(lang, list):
+        lang = [lang]
+    translator = translation(util.config_get('package'), localedir, languages=lang)
+    translator.pylons_lang = lang
+    return translator
+
 def set_lang(lang):
     """Set the i18n language used"""
     registry = pylons.request.environ['paste.registry']
     if lang is None:
         registry.replace(pylons.translator, NullTranslations())
     else:
-        import pylons.util as util
-        rootdir = pylons.g.pylons_config.paths.get('root_path')
-        localedir = os.path.join(rootdir, 'i18n')
-        if not isinstance(lang, list):
-            lang = [lang]
-        translator = translation(util.config_get('package'), localedir, languages=lang)
-        translator.pylons_lang = lang
+        translator = _get_translator(lang)
         registry.replace(pylons.translator, translator)
 
 def get_lang():
     """Return the current i18n language used"""
     return getattr(pylons.translator, 'pylons_lang', None)
+
+def add_fallback(lang):
+    """Add a fallback language from which words not matched in other languages
+    will be translated to."""
+    return pylons.translator.add_fallback(_get_translator(lang))
 
 __all__ = ['gettext_noop', 'N_', 'gettext', 'ugettext', '_', 'ngettext',
            'ungettext', 'set_lang', 'get_lang']
