e02df73a221f978f33d82496c872eda2dd1f08f2
bbangert <none@none>
2007-02-08 15:40:36 -0800

    [svn] Adding cache decorator unit tests.
    --HG--
    branch : trunk

---
+++ tests/test_units/__init__.py
@@ -0,0 +1,55 @@
+from unittest import TestCase
+from paste.wsgiwrappers import WSGIRequest
+
+import pylons
+from pylons.util import ContextObj
+
+class TestWSGIController(TestCase):
+    def setUp(self):
+        self.environ = {'pylons.routes_dict':dict(action='index'),
+                        'paste.config':dict(global_conf=dict(debug=True))}
+        pylons.c._push_object(ContextObj())
+
+    def tearDown(self):
+        pylons.request._pop_object()
+        pylons.c._pop_object()
+    
+    def get_response(self, **kargs):
+        url = kargs.pop('_url', '/')
+        self.environ['pylons.routes_dict'].update(kargs)
+        return self.app.get(url)
+
+class ControllerWrap(object):
+    def __init__(self, controller):
+        self.controller = controller
+
+    def __call__(self, environ, start_response):
+        app = self.controller()
+        app.start_response = None
+        return app(environ, start_response)
+
+class SetupCacheGlobal(object):
+    def __init__(self, app, environ, setup_g=True, setup_cache=True):
+        if setup_g:
+            g = type('G object', (object,), {})
+            g.message = 'Hello'
+            g.counter = 0
+            g.pylons_config = type('App conf', (object,), {})
+            g.pylons_config.app_conf = dict(cache_enabled='True')
+            self.g = g
+        self.app = app
+        self.environ = environ
+        self.setup_cache = setup_cache
+        self.setup_g = setup_g
+
+    def __call__(self, environ, start_response):
+        registry = environ['paste.registry']
+        if self.setup_cache:
+            registry.register(pylons.cache, environ['beaker.cache'])
+        if self.setup_g:
+            registry.register(pylons.g, self.g)
+
+        # Update the environ
+        environ.update(self.environ)
+        registry.register(pylons.request, WSGIRequest(environ))
+        return self.app(environ, start_response)
---
+++ tests/test_units/test_decorator_cache.py
@@ -0,0 +1,92 @@
+import os
+import shutil
+import time
+
+from paste.fixture import TestApp
+from paste.registry import RegistryManager
+
+from beaker.cache import CacheMiddleware
+
+import pylons
+from pylons import Response
+from pylons.decorators.cache import beaker_cache
+
+from pylons.controllers import Controller, WSGIController, XMLRPCController
+
+from __init__ import TestWSGIController, SetupCacheGlobal, ControllerWrap
+
+class CacheController(WSGIController):
+    def test_default_cache_decorator(self):
+        pylons.g.counter += 1
+        return Response('Counter=%s' % pylons.g.counter)
+    test_default_cache_decorator = beaker_cache(key=None)(test_default_cache_decorator)
+    
+    def test_get_cache_decorator(self):
+        pylons.g.counter += 1
+        return Response('Counter=%s' % pylons.g.counter)
+    test_get_cache_decorator = beaker_cache(key="param", query_args=True)(test_get_cache_decorator)
+    
+    def test_expire_cache_decorator(self):
+        pylons.g.counter += 1
+        return Response('Counter=%s' % pylons.g.counter)
+    test_expire_cache_decorator = beaker_cache(expire=8)(test_expire_cache_decorator)
+    
+    def test_key_cache_decorator(self, id):
+        pylons.g.counter += 1
+        return Response('Counter=%s, id=%s' % (pylons.g.counter, id))
+    test_key_cache_decorator = beaker_cache(key="id")(test_key_cache_decorator)
+    
+    def test_keyslist_cache_decorator(self, id, id2="123"):
+        pylons.g.counter += 1
+        return Response('Counter=%s, id=%s' % (pylons.g.counter, id))
+    test_keyslist_cache_decorator = beaker_cache(key=["id", "id2"])(test_keyslist_cache_decorator)
+
+cachedir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cache')
+
+shutil.rmtree(cachedir)
+
+environ = {}
+app = ControllerWrap(CacheController)
+app = sap = SetupCacheGlobal(app, environ)
+app = CacheMiddleware(app, {}, 
+    cache_data_dir=cachedir)
+app = RegistryManager(app)
+app = TestApp(app)
+
+class TestCacheDecorator(TestWSGIController):
+    def setUp(self):
+        self.app = app
+        TestWSGIController.setUp(self)
+        environ.update(self.environ)
+
+    def test_default_cache_decorator(self):
+        response = self.get_response(action='test_default_cache_decorator')
+        assert 'Counter=1' in response
+
+        response = self.get_response(action='test_default_cache_decorator')
+        assert 'Counter=1' in response
+        
+        response = self.get_response(action='test_get_cache_decorator', _url='/?param=123')
+        assert 'Counter=2' in response
+        response = self.get_response(action='test_get_cache_decorator', _url="/?param=123")
+        assert 'Counter=2' in response
+        
+        response = self.get_response(action='test_expire_cache_decorator')
+        assert 'Counter=3' in response
+        response = self.get_response(action='test_expire_cache_decorator')
+        assert 'Counter=3' in response
+        time.sleep(8)
+        response = self.get_response(action='test_expire_cache_decorator')
+        assert 'Counter=4' in response
+        
+        response = self.get_response(action='test_key_cache_decorator', id=1)
+        assert 'Counter=5' in response
+        response = self.get_response(action='test_key_cache_decorator', id=2)
+        assert 'Counter=6' in response
+        response = self.get_response(action='test_key_cache_decorator', id=1)
+        assert 'Counter=5' in response
+        
+        response = self.get_response(action='test_keyslist_cache_decorator', id=1, id2=2)
+        assert 'Counter=7' in response
+        response = self.get_response(action='test_keyslist_cache_decorator', id=1, id2=2)
+        assert 'Counter=7' in response
