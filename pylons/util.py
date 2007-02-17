98fb9db19fab7f52ddb26e5a63ba5795062f107b
pjenvey <none@none>
2007-02-16 23:22:09 -0800

    [svn] re-add copying __name__ from deprecated functions removed in r1588, but catch the exception raised when < Python 2.4
    --HG--
    branch : trunk

--- pylons/util.py
+++ pylons/util.py
@@ -21,6 +21,10 @@ def deprecated(func, message):
     def deprecated_method(*args, **kargs):        
         warnings.warn(message, DeprecationWarning, 2)
         return func(*args, **kargs)
+    try:
+        deprecated_method.__name__ = func.__name__
+    except TypeError: # Python < 2.4
+        pass
     deprecated_method.__doc__ = message + "\n\n" + func.__doc__
     return deprecated_method
 
