5d4ee91395f27c3ecad7515df5127e6e7dbf11f6
bbangert <none@none>
2007-03-31 15:11:20 -0700

    [svn] Updating dep requirements.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -66,10 +66,10 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "Routes>=1.6.2", "WebHelpers==dev,>=0.2.3dev-r1887", "Beaker==dev,>=0.6.3dev-r58",
+        "Routes>=1.6.2", "WebHelpers>=0.3", "Beaker>=0.6.3",
         "Paste==dev,>=1.3.1dev-r6312", "PasteDeploy==dev,>=1.1.1dev-r6282", "PasteScript==dev,>=1.3.1dev-r6311",
-        "Myghty>=1.1", "FormEncode>=0.7", "simplejson>=1.4", "decorator>=2.0.1",
-        "nose>=0.9.2"
+        "Myghty>=1.1", "FormEncode>=0.7", "simplejson>=1.7.1", "decorator>=2.0.1",
+        "nose>=0.9.2", "Mako>=0.1.5"
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
