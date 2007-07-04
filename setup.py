66f951d458c16cd1c483090b7beb93779a37b655
pjenvey <none@none>
2007-07-04 15:59:03 -0700

    [svn] whitespace
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -12,51 +12,54 @@ setup(
 Pylons
 ======
 
-The Pylons web framework is aimed at making webapps and large programmatic website
-development in Python easy. Several key points:
+The Pylons web framework is aimed at making webapps and large programmatic
+website development in Python easy. Several key points:
 
 * A framework to make writing web applications in Python easy
 
 * Inspired by Rails and TurboGears
 
-* Utilizes a minimalist, component-based philosophy that makes it easy to expand on
+* Utilizes a minimalist, component-based philosophy that makes it easy to
+  expand on
 
 * Harness existing knowledge about Python
 
 Knowing Python makes Pylons easy
 ---------------------------------
 
-Pylons makes it easy to expand on your knowledge of Python to master Pylons for web
-development. Using a MVC style dispath, Python knowledge is used at various levels:
+Pylons makes it easy to expand on your knowledge of Python to master Pylons for
+web development. Using a MVC style dispath, Python knowledge is used at various
+levels:
 
-* The Controller is just a basic Python class, called for each request. Customizing the
-  response is as easy as overriding __call__ to make your webapp work how you want.
+* The Controller is just a basic Python class, called for each
+  request. Customizing the response is as easy as overriding __call__ to make
+  your webapp work how you want.
 
-* Mako templating compiles directly to Python byte-code for speed and utilizes Python
-  for template control rather than creating its own template syntax for "for, while, etc"
+* Mako templating compiles directly to Python byte-code for speed and utilizes
+  Python for template control rather than creating its own template syntax for
+  "for, while, etc"
 
 Current Status
 ---------------
 
 Pylons %s described on this page is stable.
 
-There is also an 
-unstable `develoment version <http://pylonshq.com/svn/Pylons/trunk#egg=Pylons-dev>`_ 
-of Pylons.
+There is also an unstable `develoment version
+<http://pylonshq.com/svn/Pylons/trunk#egg=Pylons-dev>`_ of Pylons.
 
 Download and Installation
 -------------------------
 
-Pylons can be installed with `Easy Install 
+Pylons can be installed with `Easy Install
 <http://peak.telecommunity.com/DevCenter/EasyInstall>`_ by typing::
 
-    > easy_install Pylons 
+    > easy_install Pylons
 
-Dependant packages are automatically installed from 
+Dependant packages are automatically installed from
 the `Pylons download page <http://pylonshq.com/download/>`_ .
 
 
-"""%version,
+""" % version,
     keywords='web wsgi framework sqlalchemy formencode mako templates buffet',
     license='BSD',
     author='Ben Bangert, Philip Jenvey, James Gardner',
@@ -81,30 +84,30 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     extras_require = {
-        'pudge': ["docutils>=0.4", "elementtree>=1.2.6", "kid>=0.9", 
-            "Pygments>=0.7", "pudge>=0.1.3", 
-            "buildutils>=0.1.2",],
+        'pudge': ["docutils>=0.4", "elementtree>=1.2.6", "kid>=0.9",
+                  "Pygments>=0.7", "pudge>=0.1.3",
+                  "buildutils>=0.1.2"],
         'cheetah':["Cheetah>=1.0", "TurboCheetah>=0.9.5"],
         'myghty':["Myghty>=1.1"],
         'kid':["kid>=0.9", "TurboKid>=0.9.1"],
         'genshi':["Genshi>=0.3.6"],
         'full':[
-            "docutils>=0.4", "elementtree>=1.2.6", 
-            "Pygments>=0.7", "pudge>=0.1.3", 
-            "buildutils>=0.1.2", "Cheetah>=1.0", 
+            "docutils>=0.4", "elementtree>=1.2.6",
+            "Pygments>=0.7", "pudge>=0.1.3",
+            "buildutils>=0.1.2", "Cheetah>=1.0",
             "TurboCheetah>=0.9.5", "kid>=0.9", "TurboKid>=0.9.",
         ],
     },
     entry_points="""
     [paste.paster_command]
-    controller=pylons.commands:ControllerCommand
-    restcontroller=pylons.commands:RestControllerCommand
-    shell=pylons.commands:ShellCommand
+    controller = pylons.commands:ControllerCommand
+    restcontroller = pylons.commands:RestControllerCommand
+    shell = pylons.commands:ShellCommand
 
     [paste.paster_create_template]
-    pylons=pylons.util:PylonsTemplate
-    pylons_minimal=pylons.util:MinimalPylonsTemplate
-    
+    pylons = pylons.util:PylonsTemplate
+    pylons_minimal = pylons.util:MinimalPylonsTemplate
+
     [python.templating.engines]
     pylonsmyghty = pylons.templating:MyghtyTemplatePlugin [myghty]
     """,
