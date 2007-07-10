d2fe3709687f4978f3fab6083fad3758cf8c8a55
bbangert <none@none>
2007-07-09 20:24:36 -0700

    [svn] Adding dependency links and nose restriction to proper release version.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -72,7 +72,10 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
         "Routes>=1.7", "WebHelpers==dev,>=0.3.1dev-r2037", "Beaker>=0.7.3",
         "Paste>=1.3", "PasteDeploy>=1.3", "PasteScript>=1.3.2",
         "FormEncode>=0.7", "simplejson>=1.7.1", "decorator>=2.0.1",
-        "nose>=0.9.2", "Mako>=0.1.6"
+        "nose>=0.9.2,<0.10", "Mako>=0.1.6"
+    ],
+    dependency_links=[
+        "http://www.pylonshq.com/download/"
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
