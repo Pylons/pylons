be92895f4f443d75d6a9c2e6022dd26a164646ec
bbangert <none@none>
2007-08-18 13:17:22 -0700

    [svn] Added version as pylons.__version__ for scripts that need revision information.
    --HG--
    branch : trunk

--- pylons/__init__.py
+++ pylons/__init__.py
@@ -7,6 +7,22 @@ from pylons.legacy import h, jsonify, Controller, Response
 __all__ = ['c', 'g', 'cache', 'request', 'response', 'session', 'jsonify',
            'Controller', 'Response']
 
+def __figure_version():
+    try:
+        from pkg_resources import require
+        import os
+        # NOTE: this only works when the package is either installed,
+        # or has an .egg-info directory present (i.e. wont work with raw SVN checkout)
+        info = require('pylons')[0]
+        if os.path.dirname(os.path.dirname(__file__)) == info.location:
+            return info.version
+        else:
+            return '(not installed)'
+    except:
+        return '(not installed)'
+        
+__version__ = __figure_version()
+
 c = StackedObjectProxy(name="C")
 g = StackedObjectProxy(name="G")
 
