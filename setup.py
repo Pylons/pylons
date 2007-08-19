25be07299dfdcad028e4c319ecbabc13806acf76
pjenvey <none@none>
2007-08-18 18:27:26 -0700

    [svn] bump to later PasteScript
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     include_package_data=True,
     install_requires=[
         "Routes>=1.7", "WebHelpers==dev,>=0.3.2dev-r2284", "Beaker>=0.7.4",
-        "Paste>=1.4", "PasteDeploy>=1.3.1", "PasteScript==dev,>=1.3.6dev-r6751",
+        "Paste>=1.4", "PasteDeploy>=1.3.1", "PasteScript==dev,>=1.3.6dev-r6893",
         "FormEncode>=0.7", "simplejson>=1.7.1", "decorator>=2.1.0",
         "nose>=0.9.2,<=0.9.9", "Mako>=0.1.8"
     ],
