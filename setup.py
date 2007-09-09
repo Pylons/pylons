1d8aa8bf293f065e8b5855d9899860d0397d87a9
Ben Bangert <ben@groovie.org>
2007-09-08 20:03:52 -0700

    Updated Pylons dev location, changed tagging to use date.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ Current Status
 Pylons %s described on this page is stable.
 
 There is also an unstable `develoment version
-<http://pylonshq.com/svn/Pylons/trunk#egg=Pylons-dev>`_ of Pylons.
+<https://www.knowledgetap.com/hg/pylons-dev/archive/tip.tar.gz#egg=Pylons-dev>`_ of Pylons.
 
 Download and Installation
 -------------------------
