c1423dc362555329e46f07d7d2fe3f8cea7570ab
Ben Bangert <ben@groovie.org>
2007-09-10 23:06:26 -0700

    * Added nose plugin '--with-pylons=test.ini' option to load the Pylons app   before scanning for unit tests. This enables Pylons apps to be unit tested     with doc tests.
    --HG--
    branch : trunk

---
+++ pylons/test.py
@@ -0,0 +1,37 @@
+"""Nose plugin extension
+
+For use with nose to allow a project to be configured before nose proceeds
+to scan the project for doc tests and unit tests. This prevents modules from
+being loaded without a configured Pylons environment.
+
+"""
+import os
+
+from paste.deploy import loadapp
+from nose.plugins import Plugin
+
+class PylonsPlugin(Plugin):
+    enabled = False
+    enableOpt = 'pylons_config'
+    name = 'pylons'
+
+    def add_options(self, parser, env=os.environ):
+        """Add command-line options for this plugin."""
+        env_opt = 'NOSE_WITH_%s' % self.name.upper()
+        env_opt.replace('-', '_')
+        
+        parser.add_option("--with-%s" % self.name,
+                          dest=self.enableOpt, action="append",
+                          default=[],
+                          help="Setup Pylons environment with the config file"
+                          " specified by ATTR [NOSE_ATTR]")
+
+    def configure(self, options, conf):
+        self.config_file = None
+        self.conf = conf
+        if hasattr(options, self.enableOpt):
+            self.enabled = bool(getattr(options, self.enableOpt))
+            self.config_file = getattr(options, self.enableOpt)[0]
+
+    def begin(self):
+        self.app = loadapp('config:' + self.config_file, relative_to=os.getcwd())
