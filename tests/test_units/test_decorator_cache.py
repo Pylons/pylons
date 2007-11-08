44f1de7e7e697b0618bbcc3309190e8d53f43ade
Ben Bangert <ben@groovie.org>
2007-11-08 14:18:57 -0800

    Removing deprecated issues from cache test.
    --HG--
    branch : trunk

--- tests/test_units/test_decorator_cache.py
+++ tests/test_units/test_decorator_cache.py
@@ -19,32 +19,32 @@ from __init__ import data_dir, TestWSGIController, SetupCacheGlobal, \
 class CacheController(WSGIController):
     def test_default_cache_decorator(self):
         pylons.g.counter += 1
-        return Response('Counter=%s' % pylons.g.counter)
+        return 'Counter=%s' % pylons.g.counter
     test_default_cache_decorator = beaker_cache(key=None)(test_default_cache_decorator)
     
     def test_get_cache_decorator(self):
         pylons.g.counter += 1
-        return Response('Counter=%s' % pylons.g.counter)
+        return 'Counter=%s' % pylons.g.counter
     test_get_cache_decorator = beaker_cache(key="param", query_args=True)(test_get_cache_decorator)
 
     def test_get_cache_default(self):
         pylons.g.counter += 1
-        return Response('Counter=%s' % pylons.g.counter)
+        return 'Counter=%s' % pylons.g.counter
     test_get_cache_default = beaker_cache(query_args=True)(test_get_cache_default)
     
     def test_expire_cache_decorator(self):
         pylons.g.counter += 1
-        return Response('Counter=%s' % pylons.g.counter)
+        return 'Counter=%s' % pylons.g.counter
     test_expire_cache_decorator = beaker_cache(expire=1)(test_expire_cache_decorator)
     
     def test_key_cache_decorator(self, id):
         pylons.g.counter += 1
-        return Response('Counter=%s, id=%s' % (pylons.g.counter, id))
+        return 'Counter=%s, id=%s' % (pylons.g.counter, id)
     test_key_cache_decorator = beaker_cache(key="id")(test_key_cache_decorator)
     
     def test_keyslist_cache_decorator(self, id, id2="123"):
         pylons.g.counter += 1
-        return Response('Counter=%s, id=%s' % (pylons.g.counter, id))
+        return 'Counter=%s, id=%s' % (pylons.g.counter, id)
     test_keyslist_cache_decorator = beaker_cache(key=["id", "id2"])(test_keyslist_cache_decorator)
     
     def test_header_cache(self):
