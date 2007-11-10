492e77b7c354378b832e568c5980b8646ae3f63a
Mark Ramm <mark.ramm@gmail.com>
2007-11-10 14:55:56 -0500

    Moving a couple of utility classes for doing unit testing of pylons controllers into testutil module so that we can import them in tg2
    --HG--
    branch : trunk

---
+++ pylons/testutil.py
@@ -0,0 +1,52 @@
+"""Utility classes for creating workable pylons controllers for unit testing."""
+
+class ControllerWrap(object):
+    def __init__(self, controller):
+        self.controller = controller
+
+    def __call__(self, environ, start_response):
+        app = self.controller()
+        app.start_response = None
+        return app(environ, start_response)
+
+class SetupCacheGlobal(object):
+    def __init__(self, app, environ, setup_g=True, setup_cache=False,
+                 setup_session=False):
+        if setup_g:
+            g = type('G object', (object,), {})
+            g.message = 'Hello'
+            g.counter = 0
+            g.pylons_config = type('App conf', (object,), {})
+            g.pylons_config.app_conf = dict(cache_enabled='True')
+            self.g = g
+        self.app = app
+        self.environ = environ
+        self.setup_cache = setup_cache
+        self.setup_session = setup_session
+        self.setup_g = setup_g
+
+    def __call__(self, environ, start_response):
+        registry = environ['paste.registry']
+        py_obj = PylonsContext()
+        environ_config = environ.setdefault('pylons.environ_config', {})
+        if self.setup_cache:
+            py_obj.cache = environ['beaker.cache']
+            registry.register(pylons.cache, environ['beaker.cache'])
+            environ_config['cache'] = 'beaker.cache'
+        if self.setup_session:
+            py_obj.session = environ['beaker.session']
+            registry.register(pylons.session, environ['beaker.session'])
+            environ_config['session'] = 'beaker.session'
+        if self.setup_g:
+            py_obj.g = self.g
+            registry.register(pylons.g, self.g)
+
+        # Update the environ
+        environ.update(self.environ)
+        py_obj.request = req = WSGIRequest(environ)
+        py_obj.response = resp = WSGIResponse()
+        py_obj.c = ContextObj()
+        environ['pylons.pylons'] = py_obj
+        registry.register(pylons.request, req)
+        registry.register(pylons.response, resp)
+        return self.app(environ, start_response)
