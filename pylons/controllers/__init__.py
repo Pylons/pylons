22560a82d1a4f867cd0a67e4aa702c6bfd5e7ec9
Ben Bangert <ben@groovie.org>
2008-01-12 16:09:59 -0800

    Removed missing import.
    --HG--
    branch : trunk

--- pylons/controllers/__init__.py
+++ pylons/controllers/__init__.py
@@ -1,5 +1,5 @@
 """Standard Controllers intended for subclassing by web developers"""
-from pylons.controllers.core import Controller, WSGIController
+from pylons.controllers.core import WSGIController
 from pylons.controllers.xmlrpc import XMLRPCController
 from pylons.controllers.objectdispatch import ObjectDispatchController
 from pylons.controllers.decorated import DecoratedController
