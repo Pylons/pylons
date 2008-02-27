56c11ae328af7f1e7f9c3eb3fe9511a0379fa615
Ben Bangert <ben@groovie.org>
2008-02-27 14:36:21 -0800

    More dependency updates.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -72,10 +72,10 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "Routes>=1.7.1", "WebHelpers>=0.3.3", "Beaker>=0.9.2",
+        "Routes>=1.7.2", "WebHelpers>=0.3.3", "Beaker>=0.9.2",
         "Paste>=1.6", "PasteDeploy>=1.3.1", "PasteScript>=1.6.1",
-        "FormEncode>=0.7", "simplejson>=1.7.1", "decorator>=2.1.0",
-        "nose>=0.9.3", "Mako>=0.1.10", "WebOb>=0.9", "WebError>=0.8dev-20071109",
+        "FormEncode>=0.7", "simplejson>=1.7.4", "decorator>=2.1.0",
+        "nose>=0.10.1", "Mako>=0.1.10", "WebOb>=0.9", "WebError>=0.8dev-20071109",
         "Tempita>=0.1",
     ],
     dependency_links=[
