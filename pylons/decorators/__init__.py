4df7d0eda06b76c02b20857b9ba1f1d5a3d496ce
Ben Bangert <ben@groovie.org>
2008-02-28 11:24:36 -0800

    If the form content returns an error, keep it.
    --HG--
    branch : trunk

--- pylons/decorators/__init__.py
+++ pylons/decorators/__init__.py
@@ -160,7 +160,11 @@ def validate(schema=None, validators=None, form=None, variable_decode=False,
             else:
                 form_content = response
                 response = pylons.response._current_obj()
-
+            
+            # If the form_content is an exception response, return it
+            if hasattr(form_content, '_exception'):
+                return form_content
+            
             # Ensure htmlfill can safely combine the form_content, params and
             # errors variables (that they're all of the same string type)
             if not is_unicode_params:
