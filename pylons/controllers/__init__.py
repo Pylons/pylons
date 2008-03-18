f0069f6f470609458bfde4e23ab8c0ad3214a39d
Ben Bangert <ben@groovie.org>
2008-03-18 16:16:41 -0500

    Removing import of deprecated modules
    --HG--
    branch : trunk

--- pylons/controllers/__init__.py
+++ pylons/controllers/__init__.py
@@ -1,5 +1,3 @@
 """Standard Controllers intended for subclassing by web developers"""
 from pylons.controllers.core import WSGIController
 from pylons.controllers.xmlrpc import XMLRPCController
-from pylons.controllers.objectdispatch import ObjectDispatchController
-from pylons.controllers.decorated import DecoratedController
