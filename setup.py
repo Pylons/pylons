d6e5a59900ed231f7e2ff6deff3c94f97f9b6952
Ben Bangert <ben@groovie.org>
2008-04-12 11:46:49 -0700

    Bumping WebHelpers dependency.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     zip_safe=False,
     include_package_data=True,
     install_requires=[
-        "Routes>=1.8", "WebHelpers>=0.6dev-20080328", "Beaker>=0.9.3",
+        "Routes>=1.8", "WebHelpers>=0.6dev-20080410", "Beaker>=0.9.3",
         "Paste>=1.6", "PasteDeploy>=1.3.1", "PasteScript>=1.6.2",
         "FormEncode>=1.0.1", "simplejson>=1.8.1", "decorator>=2.2.0",
         "nose>=0.10.1", "Mako>=0.1.10", "WebOb>=0.9", "WebError>=0.8a",
