6c59b426ff4321305147edf6338398e239ee46ef
Ben Bangert <ben@groovie.org>
2008-05-08 22:13:51 -0700

    Initial traceback posting
    --HG--
    branch : trunk

--- pylons/media/javascripts/traceback.js
+++ pylons/media/javascripts/traceback.js
@@ -1,15 +1,41 @@
-// Establish my own namespace
-var PYREPORT = PYREPORT || {};
-
 $(document).ready(function() {
+    var getProxyJSON = function(host, path, data, callback) {
+        data['host'] = host;
+        data['path'] = path;
+        var uri = TRACEBACK.uri+'/relay';
+        $.getJSON(uri, data, callback);
+    };
+    
     // Manipulate DOM to add the iframe, and move things around
-    var iframe = $(document.createElement("iframe"));
-    parent.XMLTRACE = $('#long_xml_version textarea').html();
-    iframe.addClass('service_widget');
-    iframe[0].src = "http://127.0.0.1:5005/bugtracks/widget";
-    var newholder = $(document.createElement("div"));
-    newholder.addClass('widget_layout');
+    var newholder = $(document.createElement("div")).addClass('widget_layout');
     $('div.feature-highlight').wrapAll(newholder[0])[0].appendChild(
         $('button:last').remove()[0]);
-    iframe.appendTo('div.widget_layout');
+    $('div.widget_layout')[0].appendChild(document.getElementById('service_widget'));
+    
+    var data = {
+        q:'list:com.googlegroups.pylons-discuss list:python ' + $('code.main-exception').text(),
+        mode:'json',
+        page:1
+    };
+    
+    $('#service_widget a.submit_traceback').click(function () {
+        var uri = TRACEBACK.uri+'/post_traceback?debugcount='+debug_count
+            +'&host='+encodeURIComponent(TRACEBACK.host)
+            +'&path='+encodeURIComponent(TRACEBACK.traceback);
+        $.getJSON(uri, function(data, textStatus) {
+            var iframe = document.createElement("iframe");
+            iframe.style.display = "none";
+            document.body.appendChild(iframe);
+            iframe.src = 'http://'+TRACEBACK.host+TRACEBACK.traceback
+                +'/'+encodeURIComponent(data.traceback.link.replace(/\.xml/, ''))
+                +'/reown?uuid='+encodeURIComponent(data.traceback.uuid);
+            
+            stuff = data;
+        });
+        return false;
+    });
+    // var searchResults = getProxyJSON('markmail.org', '/results.xqy', data,
+    //     function(data, textStatus) {
+    //         stuff = data;
+    //     });
 });
