dac25bcd3d5fb30653f5806c804221a64acd1da9
Ben Bangert <ben@groovie.org>
2008-06-13 09:25:55 -0700

    Updating PasteDeploy req.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     tests_require=['nose'],
     install_requires=[
         "Routes>=1.9", "WebHelpers>=0.6dev-20080601", "Beaker>=0.9.4",
-        "Paste>=1.6", "PasteDeploy==dev,>=1.3.2dev-r7382", "PasteScript==dev,>=1.6.3dev-r7370",
+        "Paste>=1.6", "PasteDeploy>=1.3.2", "PasteScript==dev,>=1.6.3dev-r7370",
         "FormEncode>=1.0.1", "simplejson>=1.8.1", "decorator>=2.2.0",
         "nose>=0.10.3", "Mako>=0.2.0", "WebOb>=0.9.2", "WebError>=0.8",
         "Tempita>=0.2",
