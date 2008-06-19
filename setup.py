b77244b088da69332391c8c003f48343ddfe8e1d
Ben Bangert <ben@groovie.org>
2008-06-19 13:39:01 -0700

    Updating Beaker dependency
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     test_suite='nose.collector',
     tests_require=['nose'],
     install_requires=[
-        "Routes>=1.9", "WebHelpers>=0.6dev-20080613", "Beaker>=0.9.4",
+        "Routes>=1.9", "WebHelpers>=0.6dev-20080613", "Beaker>=0.9.5",
         "Paste>=1.6", "PasteDeploy>=1.3.2", "PasteScript>=1.6.3",
         "FormEncode>=1.0.1", "simplejson>=1.8.1", "decorator>=2.2.0",
         "nose>=0.10.3", "Mako>=0.2.0", "WebOb>=0.9.2", "WebError>=0.8",
