c524d35af48bc2e0481dd100d666c1821e3826c4
Ben Bangert <ben@groovie.org>
2008-07-08 14:26:19 -0700

    Bumping Mako version
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
         "Routes>=1.9.1", "WebHelpers>=0.6", "Beaker>=0.9.5",
         "Paste>=1.7.2", "PasteDeploy>=1.3.2", "PasteScript>=1.6.3",
         "FormEncode>=1.0.1", "simplejson>=1.8.1", "decorator>=2.2.0",
-        "nose>=0.10.3", "Mako>=0.2.0", "WebOb>=0.9.2", "WebError>=0.9",
+        "nose>=0.10.3", "Mako>=0.2.2", "WebOb>=0.9.2", "WebError>=0.9",
         "Tempita>=0.2",
     ],
     dependency_links=[
