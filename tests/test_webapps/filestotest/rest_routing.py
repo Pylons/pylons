d28c0e25ba9d3779a77d285e0dc82a799643e1e6
Ben Bangert <ben@groovie.org>
2008-07-21 15:53:37 -0700

    Update to reflect not having trailing slashes
    --HG--
    branch : trunk

--- tests/test_webapps/filestotest/rest_routing.py
+++ tests/test_webapps/filestotest/rest_routing.py
@@ -20,8 +20,7 @@ def make_map():
     # CUSTOM ROUTES HERE
     map.resource('restsample', 'restsamples')
 
-    map.connect('/:controller/index', action='index')
-    map.connect('/:controller/:action/')
+    map.connect('/:controller/:action')
     map.connect('/:controller/:action/:id')
 
     return map
