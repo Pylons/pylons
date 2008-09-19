a299a302d9cfb87380f2a3ea60adea3a7f421eb5
Ben Bangert <ben@groovie.org>
2008-09-19 11:22:29 -0700

    Adding Mako error formatting.
    --HG--
    branch : trunk

--- pylons/error.py
+++ pylons/error.py
@@ -4,12 +4,21 @@ Provides template engine HTML error formatters for the Template tab of
 EvalException.
 
 """
+import sys
+
 try:
     import mako.exceptions
 except ImportError:
     mako = None
 
-__all__ = []
+__all__ = ['handle_mako_error']
+
+def handle_mako_error(context, exc):
+    try:
+        exc.is_mako_exception = True
+    except:
+        pass
+    raise exc, None, sys.exc_info()[2]
 
 # Legacy support for < 0.9.7 projects
 error_template = None
@@ -27,7 +36,8 @@ template_error_formatters = [myghty_html_data]
 if mako:
     def mako_html_data(exc_value):
         """Format a Mako exception as HTML"""
-        if isinstance(exc_value, (mako.exceptions.CompileException,
+        if getattr(exc_value, 'is_mako_exception', False) or \
+           isinstance(exc_value, (mako.exceptions.CompileException,
                                   mako.exceptions.SyntaxException)):
             return mako.exceptions.html_error_template().render(full=False,
                                                                 css=False)
