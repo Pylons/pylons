9915c924f2e31c5b5df383fb2fa47036e170ff7b
Ben Bangert <ben@groovie.org>
2008-09-22 15:56:06 -0700

    Fix WebOb dep.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
         "Routes>=1.9.2", "WebHelpers>=0.6.1", "Beaker>=1.0.2",
         "Paste>=1.7.1", "PasteDeploy>=1.3.2", "PasteScript>=1.6.3",
         "FormEncode>=1.0.1", "simplejson>=1.8.1", "decorator>=2.2.0",
-        "nose>=0.10.3", "Mako>=0.2.2", "WebOb>=0.9.4", "WebError>=0.9",
+        "nose>=0.10.3", "Mako>=0.2.2", "WebOb>=0.9.3", "WebError>=0.9",
         "Tempita>=0.2",
     ],
     dependency_links=[
