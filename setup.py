98dc9bf7cc8563ecee8d52e54ea542a1443c787c
Ben Bangert <ben@groovie.org>
2008-09-22 12:24:28 -0700

    Updating dependencies
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -72,10 +72,10 @@ the `Pylons download page <http://pylonshq.com/download/>`_ .
     test_suite='nose.collector',
     tests_require=['nose'],
     install_requires=[
-        "Routes>=1.9.2", "WebHelpers>=0.6.1", "Beaker>=1.0",
+        "Routes>=1.9.2", "WebHelpers>=0.6.1", "Beaker>=1.0.2",
         "Paste>=1.7.1", "PasteDeploy>=1.3.2", "PasteScript>=1.6.3",
         "FormEncode>=1.0.1", "simplejson>=1.8.1", "decorator>=2.2.0",
-        "nose>=0.10.3", "Mako>=0.2.2", "WebOb==dev,>=0.9.3dev-r7504", "WebError>=0.9",
+        "nose>=0.10.3", "Mako>=0.2.2", "WebOb>=0.9.4", "WebError>=0.9",
         "Tempita>=0.2",
     ],
     dependency_links=[
