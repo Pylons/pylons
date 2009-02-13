16c5874dd814a122b1f0d1b68be049d0aae09e1b
Ben Bangert <ben@groovie.org>
2009-02-13 13:32:06 -0800

    Adding unit test for status code redirect
    --HG--
    branch : trunk

---
+++ tests/test_units/test_middleware.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from webtest import TestApp
+
+from pylons.middleware import StatusCodeRedirect, ErrorHandler
+
+def simple_app(environ, start_response):
+    start_response('200 OK', [('Content-type', 'text/plain')])
+    return ['Hello world!']
+
+def simple_exception_app(environ, start_response):
+    if environ['PATH_INFO'].startswith('/error/document'):
+        start_response('200 OK', [('Content-type', 'text/plain')])
+        return ['Made it to the error']
+    else:
+        start_response('404 Not Found', [('Content-type', 'text/plain')])
+        return ['No page found!']
+
+def test_plain_wrap():
+    app = TestApp(StatusCodeRedirect(simple_app))
+    res = app.get('/')
+    assert res.status_int == 200
+
+def test_status_intercept():
+    app = TestApp(StatusCodeRedirect(simple_exception_app))
+    res = app.get('/', status=404)
+    assert 'Made it to the error' in res
+
+def test_original_path():
+    app = TestApp(StatusCodeRedirect(simple_exception_app))
+    res = app.get('/', status=404)
+    assert res.environ['PATH_INFO'] == '/'
+
+def test_retains_response():
+    app = TestApp(StatusCodeRedirect(simple_exception_app))
+    res = app.get('/', status=404)
+    assert 'pylons.original_response' in res.environ
+    assert 'No page found!' in res.environ['pylons.original_response'].body
