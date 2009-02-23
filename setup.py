489c3c669c20c7269ace93d8a2e7279d0b8abd6a
Philip Jenvey <pjenvey@underboss.org>
2009-02-23 11:48:09 -0800

    bump to 0.10dev
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-version = '0.9.7'
+version = '0.10'
 
 tests_require = ['nose']
 if not sys.platform.startswith('java'):
