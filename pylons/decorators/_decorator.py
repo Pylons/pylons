1256e02a9f6c20b80b7876e4d953bfa7f20100f5
Philip Jenvey <pjenvey@underboss.org>
2009-03-02 18:59:51 -0800

    no longer needed, was for the now gone pylons.decorator
    --HG--
    branch : trunk

--- pylons/decorators/_decorator.py
+++
@@ -1,6 +0,0 @@
-"""Provides the decorator module for pylons.decorator backwards compatibility.
-
-pylons.decorator can't import the decorator module itself due to relative
-imports name clashing.
-"""
-from decorator import *
