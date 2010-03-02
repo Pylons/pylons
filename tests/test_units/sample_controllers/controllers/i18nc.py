0cf71c88a9c1875c23a2252008123f655e370f6d
Ben Bangert <ben@groovie.org>
2010-03-01 20:02:15 -0800

    Add more unit tests
    --HG--
    branch : trunk

---
+++ tests/test_units/sample_controllers/controllers/i18nc.py
@@ -0,0 +1,44 @@
+import datetime
+
+from pylons import request, response, session, url
+from pylons import tmpl_context as c
+from pylons import app_globals
+from pylons.i18n import _, get_lang, set_lang, LanguageError
+from pylons.controllers import WSGIController
+from pylons.controllers.util import abort, redirect
+
+class I18NcController(WSGIController):
+    def set_lang(self):
+        return self._set_lang(_)
+
+    def set_lang_pylonscontext(self, pylons):
+        return self._set_lang(lambda *args: pylons.translator.ugettext(*args))
+
+    def _set_lang(self, gettext):
+        lang = request.GET['lang']
+        try:
+            set_lang(lang)
+        except (LanguageError, IOError), e:
+            resp_unicode = gettext('Could not set language to "%(lang)s"') % {'lang': lang}
+        else:
+            session['lang'] = lang
+            session.save()
+            resp_unicode = gettext('Set language to "%(lang)s"') % {'lang': lang}
+        return resp_unicode
+
+    def i18n_index(self):
+        locale_list = request.languages
+        set_lang(request.languages)
+        return unicode(_('basic index page'))
+
+    def no_lang(self):
+        set_lang(None)
+        response.write(_('No language'))
+        set_lang([])
+        response.write(_('No languages'))
+        return ''
+    
+    def langs(self):
+        locale_list = request.languages
+        set_lang(request.languages)
+        return str(get_lang())
--- tests/test_units/test_basic_app.py
+++ tests/test_units/test_basic_app.py
@@ -8,21 +8,23 @@ from beaker.middleware import SessionMiddleware
 from paste.fixture import TestApp
 from paste.registry import RegistryManager
 from paste.deploy.converters import asbool
+from pylons import url
 from pylons.decorators import jsonify
 from pylons.middleware import ErrorHandler, StatusCodeRedirect
 from pylons.wsgiapp import PylonsApp
 from routes import Mapper
 from routes.middleware import RoutesMiddleware
+from routes.util import URLGenerator
 
 from nose.tools import raises
 
 
 def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
     root = os.path.dirname(os.path.abspath(__file__))
-    paths = dict(root=root, controllers=os.path.join(root, 'sample_controllers', 'controllers'))
+    paths = dict(root=os.path.join(root, 'sample_controllers'), controllers=os.path.join(root, 'sample_controllers', 'controllers'))
     sys.path.append(root)
 
-    config = configuration.PylonsConfig()
+    config = configuration.pylons_config
     config.init_app(global_conf, app_conf, package='sample_controllers', paths=paths)
     map = Mapper(directory=config['pylons.paths']['controllers'])
     map.connect('/{controller}/{action}')
@@ -49,6 +51,7 @@ def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
 class TestJsonifyDecorator(object):
     def setUp(self):
         self.app = TestApp(make_app({}))
+        url._push_object(URLGenerator(configuration.pylons_config['routes.map'], {}))
     
     def test_basic_response(self):
         response = self.app.get('/hello/index')
@@ -61,3 +64,31 @@ class TestJsonifyDecorator(object):
     def test_eval(self):
         app = TestApp(make_app(dict(debug='True')))
         app.get('/hello/oops', status=500, extra_environ={'paste.throw_errors': False})
+
+    def test_set_lang(self):
+        self._test_set_lang('set_lang')
+
+    def test_set_lang_pylonscontext(self):
+        self._test_set_lang('set_lang_pylonscontext')
+
+    def _test_set_lang(self, action):
+        response = self.app.get(url(controller='i18nc', action=action, lang='ja'))
+        assert u'\u8a00\u8a9e\u8a2d\u5b9a\u3092\u300cja\u300d\u306b\u5909\u66f4\u3057\u307e\u3057\u305f'.encode('utf-8') in response
+        response = self.app.get(url(controller='i18nc', action=action, lang='fr'))
+        assert 'Could not set language to "fr"' in response
+
+    def test_detect_lang(self):
+        response = self.app.get(url(controller='i18nc', action='i18n_index'), headers={
+                'Accept-Language':'fr;q=0.6, en;q=0.1, ja;q=0.3'})
+        # expect japanese fallback for nonexistent french.
+        assert u'\u6839\u672c\u30a4\u30f3\u30c7\u30af\u30b9\u30da\u30fc\u30b8'.encode('utf-8') in response
+
+    def test_no_lang(self):
+        response = self.app.get(url(controller='i18nc', action='no_lang'))
+        assert 'No language' in response
+        assert 'No languages' in response
+    
+    def test_langs(self):
+        response = self.app.get(url(controller='i18nc', action='langs'), headers={
+                'Accept-Language':'fr;q=0.6, en;q=0.1, ja;q=0.3'})
+        assert "['fr', 'ja', 'en', 'en-us']" in response
