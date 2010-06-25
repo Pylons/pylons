c184ed88e4af39bc0b74a950864b02b90fbe293a
Ben Bangert <ben@groovie.org>
2010-06-25 10:29:08 -0700

    Add templating tests.
    --HG--
    branch : trunk

--- tests/test_units/sample_controllers/controllers/hello.py
+++ tests/test_units/sample_controllers/controllers/hello.py
@@ -3,6 +3,7 @@ import logging
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers import WSGIController
 from pylons.controllers.util import abort, redirect
+from pylons.templating import render_mako
 from webob import Response
 from webob.exc import HTTPNotFound
 
@@ -20,6 +21,12 @@ class HelloController(WSGIController):
     
     def abort(self):
         abort(404)
+    
+    def intro_template(self):
+        return render_mako('/hello.html')
+    
+    def time_template(self):
+        return render_mako('/time.html', cache_key='fred', cache_expire=20)
 
 
 def special_controller(environ, start_response):
---
+++ tests/test_units/test_templating.py
@@ -0,0 +1,79 @@
+import os
+import re
+import sys
+
+import pylons
+import pylons.configuration as configuration
+from beaker.cache import CacheManager
+from beaker.middleware import SessionMiddleware, CacheMiddleware
+from mako.lookup import TemplateLookup
+from nose.tools import raises
+from paste.fixture import TestApp
+from paste.registry import RegistryManager
+from paste.deploy.converters import asbool
+from pylons import url
+from pylons.decorators import jsonify
+from pylons.middleware import ErrorHandler, StatusCodeRedirect
+from pylons.error import handle_mako_error
+from pylons.wsgiapp import PylonsApp
+from routes import Mapper
+from routes.middleware import RoutesMiddleware
+from routes.util import URLGenerator
+
+from nose.tools import raises
+
+
+def make_app(global_conf, full_stack=True, static_files=True, include_cache_middleware=False, attribsafe=False, **app_conf):
+    root = os.path.dirname(os.path.abspath(__file__))
+    paths = dict(root=os.path.join(root, 'sample_controllers'), controllers=os.path.join(root, 'sample_controllers', 'controllers'),
+                 templates=os.path.join(root, 'sample_controllers', 'templates'))
+    sys.path.append(root)
+
+    config = configuration.pylons_config
+    config.init_app(global_conf, app_conf, package='sample_controllers', paths=paths)
+    map = Mapper(directory=config['pylons.paths']['controllers'])
+    map.connect('/{controller}/{action}')
+    config['routes.map'] = map
+    
+    class AppGlobals(object): pass
+    
+    config['pylons.app_globals'] = AppGlobals()
+    
+    config['pylons.app_globals'].mako_lookup = TemplateLookup(
+        directories=paths['templates'], imports=['from markupsafe import escape']
+    )
+        
+    if attribsafe:
+        config['pylons.strict_tmpl_context'] = False
+    
+    app = PylonsApp(config=config)
+    app = RoutesMiddleware(app, config['routes.map'], singleton=False)
+    if include_cache_middleware:
+        app = CacheMiddleware(app, config)
+    app = SessionMiddleware(app, config)
+
+    if asbool(full_stack):
+        app = ErrorHandler(app, global_conf, **config['pylons.errorware'])
+        if asbool(config['debug']):
+            app = StatusCodeRedirect(app)
+        else:
+            app = StatusCodeRedirect(app, [401, 403, 404, 500])
+    app = RegistryManager(app)
+
+    app.config = config
+    return app
+
+class TestTemplatingApp(object):
+    def setUp(self):
+        self.app = TestApp(make_app({'cache_dir': os.path.join(os.path.dirname(__file__), 'cache')}, include_cache_middleware=True))
+        url._push_object(URLGenerator(configuration.pylons_config['routes.map'], {}))
+    
+    def test_testvars(self):
+        resp = self.app.get('/hello/intro_template')
+        assert 'Hi there 6' in resp
+    
+    def test_template_cache(self):
+        resp = self.app.get('/hello/time_template')
+        resp2 = self.app.get('/hello/time_template')
+        assert resp.body == resp2.body
+
