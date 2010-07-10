f300767b30086593f6a7925edfef48078d94ffaf
Chris McDonough <chrism@plope.com>
2010-07-10 00:45:52 -0400

    simplify legacy app
    --HG--
    branch : trunk

--- pylons/views.py
+++ pylons/views.py
@@ -1,24 +1,7 @@
-from repoze.bfg.traversal import quote_path_segment
-
 class LegacyView(object):
     def __init__(self, app):
         self.app = app # app is the legacy Pylons app
 
     def __call__(self, request):
-        traversed = request.traversed
-        vroot_path = request.virtual_root_path or ()
-        view_name = request.view_name
-        subpath = request.subpath or ()
-        script_tuple = traversed[len(vroot_path):]
-        script_list = [ quote_path_segment(name) for name in script_tuple ]
-        script_name =  '/' + '/'.join(script_list)
-        path_list = [ quote_path_segment(name) for name in subpath ] + [
-            quote_path_segment(view_name) ]
-        path_info = '/' + '/'.join(path_list)
-        request.environ['PATH_INFO'] = path_info
-        script_name = request.environ['SCRIPT_NAME'] + script_name
-        if script_name.endswith('/'):
-            script_name = script_name[:-1]
-        request.environ['SCRIPT_NAME'] = script_name
         return request.get_response(self.app)
     
--- tests/test_units/test_views.py
+++ tests/test_units/test_views.py
@@ -5,83 +5,11 @@ class LegacyViewTests(unittest.TestCase):
         from pylons.views import LegacyView
         return LegacyView(app)
 
-    def test_with_subpath_and_view_name(self):
+    def test_it(self):
         request = DummyRequest()
-        request.traversed = ['a', 'b']
-        request.virtual_root_path = ['a']
-        request.subpath = ['subpath']
-        request.view_name = 'view_name'
-        request.environ = {'SCRIPT_NAME':'/foo'}
         view = self._makeOne(dummyapp)
         response = view(request)
         self.assertEqual(response, dummyapp)
-        self.assertEqual(request.environ['PATH_INFO'], '/subpath/view_name')
-        self.assertEqual(request.environ['SCRIPT_NAME'], '/foo/b')
-        
-    def test_with_subpath_no_view_name(self):
-        request = DummyRequest()
-        request.traversed = ['a', 'b']
-        request.virtual_root_path = ['a']
-        request.subpath = ['subpath']
-        request.view_name = ''
-        request.environ = {'SCRIPT_NAME':'/foo'}
-        view = self._makeOne(dummyapp)
-        response = view(request)
-        self.assertEqual(response, dummyapp)
-        self.assertEqual(request.environ['PATH_INFO'], '/subpath/')
-        self.assertEqual(request.environ['SCRIPT_NAME'], '/foo/b')
-
-    def test_no_subpath_with_view_name(self):
-        request = DummyRequest()
-        request.traversed = ['a', 'b']
-        request.virtual_root_path = ['a']
-        request.subpath = []
-        request.view_name = 'view_name'
-        request.environ = {'SCRIPT_NAME':'/foo'}
-        view = self._makeOne(dummyapp)
-        response = view(request)
-        self.assertEqual(response, dummyapp)
-        self.assertEqual(request.environ['PATH_INFO'], '/view_name')
-        self.assertEqual(request.environ['SCRIPT_NAME'], '/foo/b')
-
-    def test_traversed_empty_with_view_name(self):
-        request = DummyRequest()
-        request.traversed = []
-        request.virtual_root_path = []
-        request.subpath = []
-        request.view_name = 'view_name'
-        request.environ = {'SCRIPT_NAME':'/foo'}
-        view = self._makeOne(dummyapp)
-        response = view(request)
-        self.assertEqual(response, dummyapp)
-        self.assertEqual(request.environ['PATH_INFO'], '/view_name')
-        self.assertEqual(request.environ['SCRIPT_NAME'], '/foo')
-
-    def test_traversed_empty_no_view_name(self):
-        request = DummyRequest()
-        request.traversed = []
-        request.virtual_root_path = []
-        request.subpath = []
-        request.view_name = ''
-        request.environ = {'SCRIPT_NAME':'/foo'}
-        view = self._makeOne(dummyapp)
-        response = view(request)
-        self.assertEqual(response, dummyapp)
-        self.assertEqual(request.environ['PATH_INFO'], '/')
-        self.assertEqual(request.environ['SCRIPT_NAME'], '/foo')
-
-    def test_traversed_empty_no_view_name_no_script_name(self):
-        request = DummyRequest()
-        request.traversed = []
-        request.virtual_root_path = []
-        request.subpath = []
-        request.view_name = ''
-        request.environ = {'SCRIPT_NAME':''}
-        view = self._makeOne(dummyapp)
-        response = view(request)
-        self.assertEqual(response, dummyapp)
-        self.assertEqual(request.environ['PATH_INFO'], '/')
-        self.assertEqual(request.environ['SCRIPT_NAME'], '')
 
 def dummyapp(environ, start_response):
     """ """
