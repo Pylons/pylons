cc964d905a928a4b0e4402628e447c23c4a4538f
Ben Bangert <ben@groovie.org>
2010-07-26 11:46:16 -0700

    Add BFG interfaces for use in Pylons
    --HG--
    branch : trunk

--- pylons/interfaces.py
+++ pylons/interfaces.py
@@ -1,6 +1,10 @@
+from repoze.bfg.interfaces import IAfterTraversal
+from repoze.bfg.interfaces import INewRequest
+from repoze.bfg.interfaces import INewResponse
+from repoze.bfg.interfaces import IWSGIApplicationCreatedEvent
 from zope.interface import Interface
 
-# private interfaces
+# internal interfaces
 
 class ITemplateGlobals(Interface):
     """ Event issued when Pylons is done loading globals """
