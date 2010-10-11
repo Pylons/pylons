10cd6c13c5ae1fafd92ac18c088556d215ef3824
Ben Bangert <ben@groovie.org>
2010-10-11 10:35:00 -0700

    Bump version to reflect what this is.
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-version = '1.1'
+version = '2.0'
 
 tests_require = ['nose', 'Jinja2>=2.2.1']
 if not sys.platform.startswith('java'):
