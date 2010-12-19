2099703bb835c4efc3c1050dcea61d1728106682
Ben Bangert <ben@groovie.org>
2010-12-18 21:06:21 -0800

    Yes, this is 1.1
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-version = '2.0a1'
+version = '1.2'
 
 tests_require = ['nose', 'Jinja2>=2.2.1']
 if not sys.platform.startswith('java'):
